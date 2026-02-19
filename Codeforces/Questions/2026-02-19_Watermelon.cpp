0e9fcdea2fb7804b0b7f0ecf947bcc7208ed144d
Supreme0019 <yash3506raj@gmail.com>
2026-02-19 17:53:44 +0530

    Solved: Watermelon on Codeforces

---
+++ Codeforces/Questions/2026-02-19_Watermelon.cpp
@@ -0,0 +1,26 @@
+/*
+Platform: Codeforces
+Problem: Watermelon
+Link: NA
+Difficulty: Easy
+Date: 2026-02-19
+*/
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if(n==2){
+        cout<<"NO"<<endl;
+    }
+    else if(n%2==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
+
