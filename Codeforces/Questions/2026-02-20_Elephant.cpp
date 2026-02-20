4e1f8c9a79d62a98632b732e4d4ef8aaa5854534
Supreme0019 <yash3506raj@gmail.com>
2026-02-20 16:18:23 +0530

    Solved: Elephant on Codeforces

---
+++ Codeforces/Questions/2026-02-20_Elephant.cpp
@@ -0,0 +1,24 @@
+/*
+Platform: Codeforces
+Problem: Elephant
+Link: na
+Difficulty: easy
+Date: 2026-02-20
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x<=5){
+        cout<<x<<endl;
+ 
+    }
+    else{
+        cout<<(x/5)+1<<endl;
+    }
+    return 0;
+}
+
