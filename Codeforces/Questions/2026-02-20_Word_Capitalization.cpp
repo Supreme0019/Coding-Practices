ffb28d38216eb28ce8cf49b808f6e6c04d678660
Supreme0019 <yash3506raj@gmail.com>
2026-02-20 14:26:29 +0530

    Solved: Word Capitalization on Codeforces

---
+++ Codeforces/Questions/2026-02-20_Word_Capitalization.cpp
@@ -0,0 +1,20 @@
+/*
+Platform: Codeforces
+Problem: Word Capitalization
+Link: na
+Difficulty: Easy
+Date: 2026-02-20
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if(islower(s[0])){
+        s[0]=toupper(s[0]);
+    }
+    cout<<s<<endl;
+}
+
