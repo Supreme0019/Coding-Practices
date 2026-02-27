d6034273d1b42c19e921383eb268d7a06fd07b6a
Supreme0019 <yash3506raj@gmail.com>
2026-02-28 00:53:54 +0530

    End of day update: Solved 8 practice problems

--- Codeforces/Questions/2026-02-28_AgainTwentyFive.cpp
+++ Codeforces/Questions/2026-02-28_AgainTwentyFive.cpp
@@ -6,4 +6,11 @@ Difficulty: Easy
 Date: 2026-02-28
 */
 
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin>>n;
+    cout<<25<<endl;
+}
---
+++ Codeforces/Questions/2026-02-28_BuyaShovel.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k,r;
+    cin>>k>>r;
+    
+}
